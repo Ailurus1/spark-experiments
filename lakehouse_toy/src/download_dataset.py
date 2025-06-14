91f80b5dd8e61783e76fe4828bc7c997f506216c
Ailurus1 <kozulin02@yandex.ru>
2025-06-14 22:14:36 +0300

    Init new experiment + EDA

---
+++ lakehouse_toy/src/download_dataset.py
@@ -0,0 +1,20 @@
+import kagglehub
+from pathlib import Path
+
+
+def download_dataset():
+    destination = Path("data")
+    destination.mkdir(exist_ok=True)
+    target = destination / "data.csv"
+
+    if not target.exists():
+        path = Path(kagglehub.dataset_download("parisrohan/credit-score-classification"))
+        source = path / "train.csv"
+        target.write_bytes(source.read_bytes())
+        print(f"Dataset downloaded to {target}")
+    else:
+        print(f"Dataset already exists at {target}")
+
+
+if __name__ == "__main__":
+    download_dataset()
